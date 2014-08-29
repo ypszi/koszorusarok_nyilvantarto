aa9a6639820e6fbb1f17e9e67ae6caa77752e89d
ypszi <pecosz.p@gmail.com>
2014-08-29 14:53:09 +0200

    gallery fix

--- menu-main/menu-setting-submenu/wreaths/wreathmod.php
+++ menu-main/menu-setting-submenu/wreaths/wreathmod.php
@@ -9,6 +9,13 @@
 	$wreath_name = $_POST['wreath_name'];
 	$the_same_wname = false;
 
+	$wreath_id = 0; //Módosítandó koszorú ID-je
+	$query_wid = "SELECT id FROM `special_wreath` WHERE name = '".$_POST['origin_wname']."'";
+	$result_wid = mysql_query($query_wid, $conn) or die(mysql_error());
+	while ($row = mysql_fetch_assoc($result_wid)) {
+		$wreath_id = $row['id'];
+	}
+
 	$save_errors = 0;
 
 	$is_wreath = "SELECT name FROM special_wreath WHERE name='$wreath_name'";
@@ -54,159 +61,154 @@
 		if (isset($_POST['rezgoqty'])) $rqty = $_POST['rezgoqty']; else $rqty = 0;
 
 		//Kép feltöltés
-		$file_name = $_FILES["file"]["name"];
-		$file_type = $_FILES["file"]["type"];
-		$file_size = $_FILES["file"]["size"];
-		$accents = array(
-		        'á' => 'a', 'Á' => 'A',
-		        'ä' => 'a', 'Ä' => 'A',
+		if (isset($_FILES['file']) === true) {
+			$file_name = $_FILES["file"]["name"];
+			$file_type = $_FILES["file"]["type"];
+			$file_size = $_FILES["file"]["size"];
+			$accents = array(
+			        'á' => 'a', 'Á' => 'A',
+			        'ä' => 'a', 'Ä' => 'A',
 
-		        'é' => 'e', 'É' => 'E',
+			        'é' => 'e', 'É' => 'E',
 
-		        'í' => 'i', 'Í' => 'I',
+			        'í' => 'i', 'Í' => 'I',
 
-		        'ó' => 'o', 'Ó' => 'O',
-		        'ö' => 'o', 'Ö' => 'O',
-		        'ő' => 'o', 'Ő' => 'O',
+			        'ó' => 'o', 'Ó' => 'O',
+			        'ö' => 'o', 'Ö' => 'O',
+			        'ő' => 'o', 'Ő' => 'O',
 
-		        'ú' => 'u', 'Ú' => 'U',
-		        'ü' => 'u', 'Ü' => 'U',
-		        'ű' => 'u', 'Ű' => 'U',
-		        ' ' => ''
-		);
+			        'ú' => 'u', 'Ú' => 'U',
+			        'ü' => 'u', 'Ü' => 'U',
+			        'ű' => 'u', 'Ű' => 'U',
+			        ' ' => ''
+			);
 
-		foreach ($accents as $key => $val)
-		{
-		    $file_name = preg_replace('#'.$key.'#', $val, $file_name);
-		}
-
-		$allowedExts = array("gif", "GIF", "jpeg", "JPEG", "jpg", "JPG", "png", "PNG");
-		$exploded = explode(".", $file_name);
-		$extension = end($exploded);
-		$target = "../../../img/wreath/";
-		$maxsize = 10485760; // 10MB -> 10240kbyte * 1024byte
-
-		if ((($file_type == "image/gif")
-			|| ($file_type == "image/jpeg")
-			|| ($file_type == "image/jpg")
-			|| ($file_type == "image/pjpeg")
-			|| ($file_type == "image/x-png")
-			|| ($file_type == "image/png"))
-			&& ($file_size < $maxsize)
-			&& in_array($extension, $allowedExts)) {
-
-			if ($_FILES["file"]["error"] > 0) {
-				$save_errors++;
-				echo "<script type=\"text/javascript\">
-					document.getElementById('alertwindow').innerHTML = '<h1>Hiba! Hibakód: " . $_FILES["file"]["error"] . "</h1>';
-					document.getElementById('alertwindow').style.display = 'block';
-					</script>";
+			foreach ($accents as $key => $val)
+			{
+			    $file_name = preg_replace('#'.$key.'#', $val, $file_name);
 			}
-			else {
 
-				if (file_exists($target . $file_name)) {
+			$allowedExts = array("gif", "GIF", "jpeg", "JPEG", "jpg", "JPG", "png", "PNG");
+			$exploded = explode(".", $file_name);
+			$extension = end($exploded);
+			$target = "../../../img/wreath/";
+			$maxsize = 10485760; // 10MB -> 10240kbyte * 1024byte
+
+			if ((($file_type == "image/gif")
+				|| ($file_type == "image/jpeg")
+				|| ($file_type == "image/jpg")
+				|| ($file_type == "image/pjpeg")
+				|| ($file_type == "image/x-png")
+				|| ($file_type == "image/png"))
+				&& ($file_size < $maxsize)
+				&& in_array($extension, $allowedExts)) {
+
+				if ($_FILES["file"]["error"] > 0) {
 					$save_errors++;
 					echo "<script type=\"text/javascript\">
-					document.getElementById('alertwindow').innerHTML = '<h1>Ez a f&aacute;jl m&aacute;r l&eacute;tezik: " . $file_name . "! </h1>';
-					document.getElementById('alertwindow').style.display = 'block';
-					</script>";
+						document.getElementById('alertwindow').innerHTML = '<h1>Hiba! Hibakód: " . $_FILES["file"]["error"] . "</h1>';
+						document.getElementById('alertwindow').style.display = 'block';
+						</script>";
 				}
 				else {
-					move_uploaded_file($_FILES["file"]["tmp_name"], $target . $file_name);
-					echo "<script type=\"text/javascript\">
-					document.getElementById('alertwindow').innerHTML = '<h1>K&eacute;pfelt&ouml;lt&eacute;s sikeres!</h1>';
-					document.getElementById('alertwindow').style.display = 'block';
-					</script>";
-					//echo "File stored in: " . "upload/" . $file_name;
-				}
-			}
-		}
-		else {
-			if ($file_size > $maxsize) {
-				echo "<script type=\"text/javascript\">
-					document.getElementById('alertwindow').innerHTML = '<h1>T&uacute;l nagy f&aacute;jlm&eacute;ret!</h1>';
-					document.getElementById('alertwindow').style.display = 'block';
-					</script>";
-			} else {
-				if ($file_name != "") {
-					$save_errors++;
-					echo "<script type=\"text/javascript\">
-					document.getElementById('alertwindow').innerHTML = '<h1>&eacute;rv&eacute;nytelen f&aacute;jl!</h1>';
-					document.getElementById('alertwindow').style.display = 'block';
-					</script>";
-				}
-			}
-		}
 
-		if ($file_name == ""){
-			$picture = $_POST['current_img'];
-		} else {
-			$picture = $file_name;
+					if (file_exists($target . $file_name)) {
+						$save_errors++;
+						echo "<script type=\"text/javascript\">
+						document.getElementById('alertwindow').innerHTML = '<h1>Ez a f&aacute;jl m&aacute;r l&eacute;tezik: " . $file_name . "! </h1>';
+						document.getElementById('alertwindow').style.display = 'block';
+						</script>";
+					} else {
+						move_uploaded_file($_FILES["file"]["tmp_name"], $target . $file_name);
+						echo "<script type=\"text/javascript\">
+						document.getElementById('alertwindow').innerHTML = '<h1>K&eacute;pfelt&ouml;lt&eacute;s sikeres!</h1>';
+						document.getElementById('alertwindow').style.display = 'block';
+						</script>";
+						//echo "File stored in: " . "upload/" . $file_name;
+					}
+				}
+			} else {
+				if ($file_size > $maxsize) {
+					echo "<script type=\"text/javascript\">
+						document.getElementById('alertwindow').innerHTML = '<h1>T&uacute;l nagy f&aacute;jlm&eacute;ret!</h1>';
+						document.getElementById('alertwindow').style.display = 'block';
+						</script>";
+				} else {
+					if ($file_name != "") {
+						$save_errors++;
+						echo "<script type=\"text/javascript\">
+						document.getElementById('alertwindow').innerHTML = '<h1>&eacute;rv&eacute;nytelen f&aacute;jl!</h1>';
+						document.getElementById('alertwindow').style.display = 'block';
+						</script>";
+					}
+				}
+			}
+
+			if ($file_name == ""){
+				$picture = $_POST['current_img'];
+			} else {
+				$picture = $file_name;
+			}
 		}
 
 		// Galéria feltöltés
 		$filenames = array();
 		if (isset($_FILES['swreath_img_gallery']) === true) {
-		  $files = $_FILES['swreath_img_gallery'];
+			$files = $_FILES['swreath_img_gallery'];
 
-		  for ($i=0; $i < count($files['name']); $i++) {
-			$filename = $files['name'][$i];
-			$ftmp_name = $files['tmp_name'][$i];
-			$filetype = $files['type'][$i];
-			$filesize = $files['size'][$i];
+			if (count($files['name']) <= 5) {
+				for ($i=0; $i < count($files['name']); $i++) {
+					$filename = $files['name'][$i];
+					$ftmp_name = $files['tmp_name'][$i];
+					$filetype = $files['type'][$i];
+					$filesize = $files['size'][$i];
 
-			$accents = array(
-						'á' => 'a', 'Á' => 'A',
-						'ä' => 'a', 'Ä' => 'A',
+					$accents = array(
+							'á' => 'a', 'Á' => 'A',
+							'ä' => 'a', 'Ä' => 'A',
 
-						'é' => 'e', 'É' => 'E',
+							'é' => 'e', 'É' => 'E',
 
-						'í' => 'i', 'Í' => 'I',
+							'í' => 'i', 'Í' => 'I',
 
-						'ó' => 'o', 'Ó' => 'O',
-						'ö' => 'o', 'Ö' => 'O',
-						'ő' => 'o', 'Ő' => 'O',
+							'ó' => 'o', 'Ó' => 'O',
+							'ö' => 'o', 'Ö' => 'O',
+							'ő' => 'o', 'Ő' => 'O',
 
-						'ú' => 'u', 'Ú' => 'U',
-						'ü' => 'u', 'Ü' => 'U',
-						'ű' => 'u', 'Ű' => 'U',
-						' ' => '' );
+							'ú' => 'u', 'Ú' => 'U',
+							'ü' => 'u', 'Ü' => 'U',
+							'ű' => 'u', 'Ű' => 'U',
+							' ' => '' );
 
-				foreach ($accents as $key => $val)
-				{
-					$filename = preg_replace('#'.$key.'#', $val, $filename);
-				}
+					foreach ($accents as $key => $val)
+					{
+						$filename = preg_replace('#'.$key.'#', $val, $filename);
+					}
 
-				$allowedExts = array("gif", "GIF", "jpeg", "JPEG", "jpg", "JPG", "png", "PNG");
-				$exploded = explode(".", $filename);
-				$extension = end($exploded);
-				$target = "../../../img/wreath/";
-				$maxsize = 10485760; // 10MB -> 10240kbyte * 1024byte
+					$allowedExts = array("gif", "GIF", "jpeg", "JPEG", "jpg", "JPG", "png", "PNG");
+					$exploded = explode(".", $filename);
+					$extension = end($exploded);
+					$target = "../../../img/gallery/";
+					$maxsize = 10485760; // 10MB -> 10240kbyte * 1024byte
 
-				if ((($filetype == "image/gif")
-				|| ($filetype == "image/jpeg")
-				|| ($filetype == "image/jpg")
-				|| ($filetype == "image/pjpeg")
-				|| ($filetype == "image/x-png")
-				|| ($filetype == "image/png"))
-				&& ($filesize < $maxsize)
-				&& in_array($extension, $allowedExts)) {
+					if ((($filetype == "image/gif")
+					|| ($filetype == "image/jpeg")
+					|| ($filetype == "image/jpg")
+					|| ($filetype == "image/pjpeg")
+					|| ($filetype == "image/x-png")
+					|| ($filetype == "image/png"))
+					&& ($filesize < $maxsize)
+					&& in_array($extension, $allowedExts)) {
 
-					if ($files["error"][$i] > 0) {
-						$save_errors++;
-						echo "<script type=\"text/javascript\">
-							document.getElementById('alertwindow').innerHTML = '<h1>Hiba! Hibakód: " . $files["error"][$i] . "</h1>';
-							document.getElementById('alertwindow').style.display = 'block';
-							</script>";
-					} else {
-						if (file_exists($target . $filename)) {
+						if ($files["error"][$i] > 0) {
 							$save_errors++;
 							echo "<script type=\"text/javascript\">
-							document.getElementById('alertwindow').innerHTML = '<h1>Ez a f&aacute;jl m&aacute;r l&eacute;tezik: " . $filename . "! </h1>';
-							document.getElementById('alertwindow').style.display = 'block';
-							</script>";
-						}
-						else {
+								document.getElementById('alertwindow').innerHTML = '<h1>Hiba! Hibakód: " . $files["error"][$i] . "</h1>';
+								document.getElementById('alertwindow').style.display = 'block';
+								</script>";
+						} else {
+							// ha már létezik a fájl, akkor töröljük
+							unlink( $target . $filename );
 							$filenames[] = $filename;
 							move_uploaded_file($ftmp_name, $target . $filename);
 							echo "<script type=\"text/javascript\">
@@ -215,25 +217,30 @@
 							</script>";
 							//echo "File stored in: " . "upload/" . $fname;
 						}
-					}
-				}
-				else {
-					if ($filesize > $maxsize) {
-						$save_errors++;
-						echo "<script type=\"text/javascript\">
-							document.getElementById('alertwindow').innerHTML = '<h1>T&uacute;l nagy f&aacute;jlm&eacute;ret!</h1>';
-							document.getElementById('alertwindow').style.display = 'block';
-							</script>";
 					} else {
-						if ($filename != "") {
+						if ($filesize > $maxsize) {
 							$save_errors++;
 							echo "<script type=\"text/javascript\">
-							document.getElementById('alertwindow').innerHTML = '<h1>&eacute;rv&eacute;nytelen f&aacute;jl!</h1>';
-							document.getElementById('alertwindow').style.display = 'block';
-							</script>";
+								document.getElementById('alertwindow').innerHTML = '<h1>T&uacute;l nagy f&aacute;jlm&eacute;ret!</h1>';
+								document.getElementById('alertwindow').style.display = 'block';
+								</script>";
+						} else {
+							if ($filename != "") {
+								$save_errors++;
+								echo "<script type=\"text/javascript\">
+								document.getElementById('alertwindow').innerHTML = '<h1>&eacute;rv&eacute;nytelen f&aacute;jl!</h1>';
+								document.getElementById('alertwindow').style.display = 'block';
+								</script>";
+							}
 						}
 					}
 				}
+			} else {
+				$save_errors++;
+				echo "<script type=\"text/javascript\">
+				document.getElementById('alertwindow').innerHTML = '<h1>T&uacute;l sok f&aacute;jl!</h1>';
+				document.getElementById('alertwindow').style.display = 'block';
+				</script>";
 			}
 		}
 
@@ -243,18 +250,11 @@
 
 		if ($save_errors > 0) {
 			echo "<script type=\"text/javascript\">
-			document.getElementById('alertwindow').innerHTML += '<h1>Sikertelen hozzáadás!</h1>';
+			document.getElementById('alertwindow').innerHTML += '<h1>Sikertelen szerkeszt&eacute;s!</h1>';
 			document.getElementById('alertwindow').style.display = 'block';
 			setTimeout('window.location.href=\"../../../index.php?page=beallitas&subpage=koszoru&koszoru_tipus=5\"', 2000);
 			</script>";
 		} else {
-
-			$wreath_id = 0; //Módosítandó koszorú ID-je
-			$query_wid = "SELECT id FROM `special_wreath` WHERE name = '".$_POST['origin_wname']."'";
-			$result_wid = mysql_query($query_wid, $conn) or die(mysql_error());
-			while ($row = mysql_fetch_assoc($result_wid)) {
-				$wreath_id = $row['id'];
-			}
 
 			$query_id = "SELECT id FROM `base_wreath` WHERE size = '$base_wreath_size'";
 			$result_id = mysql_query($query_id, $conn) or die(mysql_error());
@@ -271,14 +271,14 @@
 				$result_url = mysql_query($query_url) or die (mysql_error());
 
 				while ($row = mysql_fetch_assoc($result_url)) {
-					unlink( "../../../img/wreath/".basename($row['url']) );
+					unlink( "../../../img/gallery/".basename($row['url']) );
 				}
 
 				$query_del = "DELETE FROM `special_wreath_img` WHERE `special_wreath_id`=$wreath_id";
 				$result_del = mysql_query($query_del) or die (mysql_error());
 
 				foreach ($filenames as $filename) {
-					$url = $conf_path_abs . "img/wreath/". $filename;
+					$url = $conf_path_abs . "img/gallery/". $filename;
 					$query = "INSERT INTO `special_wreath_img` (`special_wreath_id`, `url`) VALUES ($wreath_id, '$url') ";
 					$result = mysql_query($query, $conn) or die(mysql_error());
 				}
@@ -328,7 +328,7 @@
 			}
 
 			echo "<script type=\"text/javascript\">
-				document.getElementById('alertwindow').innerHTML = '<h1>Sikeres módosítás!</h1>';
+				document.getElementById('alertwindow').innerHTML = '<h1>Sikeres m&oacute;dos&iacute;t&aacute;s!</h1>';
 				document.getElementById('alertwindow').style.display = 'block';
 				setTimeout('window.location.href=\"../../../index.php?page=beallitas&subpage=koszoru\"', 1000);</script>";
 		}
