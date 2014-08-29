28aacb1d520365fd9b30f3e56a355da0f8a877b2
ypszi <pecosz.p@gmail.com>
2014-08-29 17:07:01 +0200

    galeria torles koszoru torleskor

--- menu-main/menu-setting-submenu/wreaths/del_special_wreath.php
+++ menu-main/menu-setting-submenu/wreaths/del_special_wreath.php
@@ -8,3 +8,6 @@
 
 	$query_del = "DELETE FROM `conect_flower_special_wreath` WHERE `special_wreath_id`=$id";
 	mysql_query($query_del) or die (mysql_error());
+
+	$query_del = "DELETE FROM `special_wreath_img` WHERE `special_wreath_id`=$id";
+	mysql_query($query_del) or die (mysql_error());
